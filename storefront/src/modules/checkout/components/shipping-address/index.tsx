23f19d575f2a5b68a059b9ca4aaa9a334f49dd2c
Phạm Xuân Bách <pxuanbach1412@gmail.com>
2024-05-25 11:06:04 +0700

    fix type error

--- storefront/src/modules/checkout/components/shipping-address/index.tsx
+++ storefront/src/modules/checkout/components/shipping-address/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from "react"
-import { Address, Cart, Customer } from "@medusajs/medusa"
+import { Address, Cart, Country, Customer } from "@medusajs/medusa"
 import Checkbox from "@modules/common/components/checkbox"
 import Input from "@modules/common/components/input"
 import AddressSelect from "../address-select"
@@ -34,7 +34,7 @@ const ShippingAddress = ({
   })
 
   const countriesInRegion = useMemo(
-    () => cart?.region.countries.map((c) => c.iso_2),
+    () => cart?.region.countries.map((c: Country) => c.iso_2),
     [cart?.region]
   )
 
